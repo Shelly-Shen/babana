8ef8afdbd7f8a95cb6978426d225ac20b10459de
Shelly-Shen <18846150448@163.com>
2018-06-26 12:33:06 +0800

    添加注册页

--- vuetest/src/components/Header.vue
+++ vuetest/src/components/Header.vue
@@ -1,10 +1,11 @@
 <template> 
-	<div class="container"> 
-		<el-menu :router="true" :default-axctive="active" class="el-menu" mode="horizontal" @select="handleSelect">
-			<el-menu-item index="/">主页</el-menu-item>
-			<el-menu-item index="/List"><i class="fa fa-flag" aria-hidden="true"></i>探索</el-menu-item>		
-			<el-menu-item index="4" class="right"><i class="fa fa-user-o" aria-hidden="true"></i>注册</el-menu-item>
-			<el-menu-item index="3" class="right"><i class="fa fa-key" aria-hidden="true"></i>登陆</el-menu-item>
+  <div class="container">
+    <el-menu :router="true" default-active="/" class="el-menu" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="/">番薯</el-menu-item>
+      <el-menu-item index="/List"><i class="fa fa-flag" aria-hidden="true"></i> 探索</el-menu-item>
+
+        <el-menu-item index="signUp" class="right"><i class="fa fa-user-o" aria-hidden="true"></i> 注册</el-menu-item>
+        <el-menu-item index="3" class="right"><i class="fa fa-key" aria-hidden="true"></i> 登陆</el-menu-item>
 		</el-menu>
 	</div>
 </template>
@@ -13,14 +14,24 @@
 		name:'Header',
 		data(){
 			return{
-			active:'0'
+			active:'/'
 			};
 		},
+			created(){
+		    this.active = this.$route.path; // 解决刷新不高亮
+		    this.$router.afterEach((to,from) => {
+		    	console.log(to.path);
+		      this.active = to.path; 
+		      		    	console.log(this.active)
+		      // 解决编程式切换路由不高亮
+		    });
+		  },
 		methods:{
 			handleSelect(key,keyPath){
 				console.log(key,keyPath)
 			}
-		}
+		},
+	
 	}
 </script>
 <style lang="css" scoped>
--- vuetest/src/router/index.js
+++ vuetest/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import List from '@/page/List'
+import signUp from '@/page/signUp'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +16,10 @@ export default new Router({
     	path:'/list',
     	name:'List',
     	component:List
+    },{
+    	path:'/signUp',
+    	name:'signUp',
+    	component:signUp
     }
   ]
 })
